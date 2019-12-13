1252021c41cb0b7192e09eb0bf4b649504c5b569
HugoVN <hugo_j_alexander@hotmail.com>
2019-12-13 10:06:12 -0500

    I had a hard time with stacked histograms

---
+++ data/canMedals2.py
@@ -0,0 +1,43 @@
+import csv
+import matplotlib.pyplot as plt
+
+golds = []
+silvers = []
+bronzes = []
+bins = [1924,1950,1975,2000,2025]
+colors = ['gold', 'silver', 'darkgoldenrod']
+labels = ['Gold', 'Silver', 'Bronze']
+
+categories = [] # first row -> not data
+
+with open('mensMedals.csv') as csvfile:
+	reader = csv.reader(csvfile)
+	line_count = 0
+
+	for row in reader:
+		if line_count == 0:
+			print("this is the first row in the spreadsheet")
+			categories.append(row)
+			line_count += 1
+
+		else:
+			if row[7] == "Gold":
+				print("won a gold medal")
+				golds.append(row[0])
+			elif row[7] == "Silver":
+				print("won a silver medal")
+				silvers.append(row[0])
+			else:
+				print("won a bronze medal")
+				bronzes.append(row[0])
+
+			print(line_count)
+			line_count += 1
+plt.figure()
+plt.hist([golds,silvers,bronzes], edgecolor='black', stacked=True, color=["red", "blue", "violet"], density=True)
+
+plt.legend(labels, loc=1)
+plt.xlabel('years')
+plt.ylabel('Number of medals')
+plt.title('Canadian Medals')
+plt.show()
---
+++ data/canMedals2004.py
@@ -0,0 +1,60 @@
+import csv
+
+import matplotlib.pyplot as plt
+
+
+can = []
+others = []
+
+categories = []
+
+
+with open('OlympicsWinter1994-2004.csv') as csvfile:
+	reader = csv.reader(csvfile)
+	line_count = 0
+
+
+
+	for row in reader:
+
+		if line_count == 0:
+
+			print("this is the first row in the spreadsheet")
+			categories.append(row)
+			line_count += 1
+
+		else:
+
+			if row[4] == "CAN":
+				print("canada won")
+				can.append(row[4])
+
+			else:
+				print("others won")
+				others.append(row[4])
+
+
+			line_count += 1;
+
+
+# now we can see our medal counts
+print(len(can), "medals for Canada")
+print(len(others), "medals for Others")
+
+
+
+#plot a pie chart
+
+labels = ("Wins", "Loses")
+colors = ['red', 'dimgrey']
+explode = (0.1, 0.1)
+sizes = [len(can), len(others)]
+
+plt.pie(sizes, explode=explode, colors=colors, autopct='%1.f%%', shadow=True, startangle=90)
+
+plt.axis('equal')
+
+#plt.legend(labels, loc=1)
+#plt.title("Canadian Medal Wins")
+plt.xlabel("1994-2004")
+plt.show()
